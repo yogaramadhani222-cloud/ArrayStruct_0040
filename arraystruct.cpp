c55639ca1b95e1bac41cdf90baf6d20d529fcd97
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-04-06 12:46:45 +0700

    deklarasi array disetai inisialisasi

--- arraystruct.cpp
+++ arraystruct.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+int main() 
+{
+    // deklarasi array disertai inisialisasi
+    int dataku[5] = {10, 20, 30, 40, 50};
+    // deklarasi array tanpa inisialisasi
+    int datamu[5];
+}
