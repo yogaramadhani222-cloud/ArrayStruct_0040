f593ed7ea6d2e6560d4451615dab43ea08ad150e
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-04-06 12:50:16 +0700

    mengisi array

--- arraystruct.cpp
+++ arraystruct.cpp
@@ -7,4 +7,10 @@ int main()
     int dataku[5] = {10, 20, 30, 40, 50};
     // deklarasi array tanpa inisialisasi
     int datamu[5];
+
+    // cara akses (membaca atau mengambil isi array)
+    cout << "isi data kedua : " << dataku[1] << endl;
+    // mengisi array
+    dataku[1] = 200;
+    cout << "isi data kedua setelah dirubah : " << dataku[1] << endl;
 }
