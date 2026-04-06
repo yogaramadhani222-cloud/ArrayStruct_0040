154ee5e108c648b961a5ce78e8bb6caab601cba3
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-04-06 13:00:24 +0700

    mengisi perintah dan menampilkan perintah

--- arraystruct2.cpp
+++ arraystruct2.cpp
@@ -20,4 +20,23 @@ int main()
     // mengisi array
     dataku[1] = 200;
     cout << "isi data kedua setelah dirubah : " << dataku[1] << endl;
+
+    // mengisi data dengan perintah satu persatu
+    cout << "isikan data pertama : ";
+    cin >> datamu[0];
+    cout << "isikan data kedua : ";
+    cin >> datamu[1];
+    cout << "isikan data ketiga : ";
+    cin >> datamu[2];
+    cout << "isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "isikan data kelima : ";
+    cin >> datamu[4];
+
+    // menampilkan data dengan perintah satu persatu
+    cout << "data pertama : " << datamu[0] << endl;
+    cout << "data kedua : " << datamu[1] << endl;
+    cout << "data ketiga : " << datamu[2] << endl;
+    cout << "data keempat : " << datamu[3] << endl;
+    cout << "data kelima : " << datamu[4] << endl;
 }
